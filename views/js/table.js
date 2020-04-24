de458562a1a9d02a253c8d8f16d74b7b087c51e8
jsous7 <juliana.oli.sousa@gmail.com>
2020-04-24 19:02:24 +0100

    disabling non-used scripts, creating extra html input fields

--- views/js/table.js
+++ views/js/table.js
@@ -1,61 +1,61 @@
-/** All the code and subroutines are based on the code of Mikhail Timofeev,
- * the reference code is on github: https://github.com/mikhail-cct/CA1-In-class-Demo.git */
+// /** All the code and subroutines are based on the code of Mikhail Timofeev,
+//  * the reference code is on github: https://github.com/mikhail-cct/CA1-In-class-Demo.git */
 
-$(document).ready(function(){
-    draw_table();
-})
+// $(document).ready(function(){
+//     draw_table();
+// })
 
-function draw_table()
-{
-    $("#results").empty();
-    $.getJSONuncached = function (url)
-    {
-        return $.ajax(
-            {
-                url: '/services',
-                type: 'GET',
-                cache: false,
-                success: function (html)
-                {
-                    $("#results").append(html);
-                    select_row();
-                }
-            });
-    };
-    $.getJSONuncached("/read")
-};
+// function draw_table()
+// {
+//     $("#results").empty();
+//     $.getJSONuncached = function (url)
+//     {
+//         return $.ajax(
+//             {
+//                 url: '/services',
+//                 type: 'GET',
+//                 cache: false,
+//                 success: function (html)
+//                 {
+//                     $("#results").append(html);
+//                     select_row();
+//                 }
+//             });
+//     };
+//     $.getJSONuncached("/read")
+// };
 
-function select_row()
-{
-    $("#menuTable tbody tr[id]").click(function ()
-    {
-        //create css class "selected" dynamically on the table rows
-        $(".selected").removeClass("selected");
-        $(this).addClass("selected");
+// function select_row()
+// {
+//     $("#menuTable tbody tr[id]").click(function ()
+//     {
+//         //create css class "selected" dynamically on the table rows
+//         $(".selected").removeClass("selected");
+//         $(this).addClass("selected");
 
-        var service = $(this).prevAll("tr").children("td[colspan='4']").length - 1;
-        var entree = $(this).attr("id") - 1;
-        console.log(service);
-        console.log(entree);
-        delete_row(service, entree);
-    })
-};
+//         var service = $(this).prevAll("tr").children("td[colspan='4']").length - 1;
+//         var entree = $(this).attr("id") - 1;
+//         console.log(service);
+//         console.log(entree);
+//         delete_row(service, entree);
+//     })
+// };
 
-function delete_row(service, entree)
-{
-    $("#delete").click(function ()
-    {
-        $.ajax(
-            {
-                url: "/delete",
-                type: "POST",
-                data:
-                    {
-                        section: service,
-                        entree: entree
-                    },
-                cache: false,
-                success: setTimeout(draw_table, 1000)
-            })
-    })
-};
+// function delete_row(service, entree)
+// {
+//     $("#delete").click(function ()
+//     {
+//         $.ajax(
+//             {
+//                 url: "/delete",
+//                 type: "POST",
+//                 data:
+//                     {
+//                         section: service,
+//                         entree: entree
+//                     },
+//                 cache: false,
+//                 success: setTimeout(draw_table, 1000)
+//             })
+//     })
+// };
