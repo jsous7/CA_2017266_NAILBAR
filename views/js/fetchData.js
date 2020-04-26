0f4379b84ec9f3abddecb42004142dbf52bccd79
jsous7 <juliana.oli.sousa@gmail.com>
2020-04-26 20:13:19 +0100

    Implementing logic for total price and select table

--- views/js/fetchData.js
+++ views/js/fetchData.js
@@ -1,46 +1,26 @@
 (function() {
     jsonGet('/services').then(function(serverResponse){
         for (i in serverResponse.result) {
+            let tbodyTemplate = 
+            `<tr data-id="${serverResponse.result[i]._id}" data-price="${serverResponse.result[i].price}">
+                <td>${serverResponse.result[i].item}</td>
+                <td>${serverResponse.result[i].description}</td>
+                <td>${serverResponse.result[i].price}</td>
+                <td>
+                    <button type="submit" id="edit" onclick="editService('${serverResponse.result[i]._id}')" class="btn btn-block btn-primary custom-buttom">Edit</button>
+                    <button type="submit" id="delete" onclick="deleteService('${serverResponse.result[i]._id}')" class="btn btn-block btn-danger custom-buttom">Delete</button>
+                </td>
+            </tr>`;
             if (serverResponse.result[i].type === 'manicure') {
-                $('#table_body_manicure').append(
-                    `<tr>
-                        <td>${serverResponse.result[i].item}</td>
-                        <td>${serverResponse.result[i].description}</td>
-                        <td>${serverResponse.result[i].price}</td>
-                        <td>
-                            <button type="submit" id="edit" onclick="editService('${serverResponse.result[i]._id}')" class="btn btn-block btn-primary custom-buttom">Edit</button>
-                            <button type="submit" id="delete" onclick="deleteService('${serverResponse.result[i]._id}')" class="btn btn-block btn-danger custom-buttom">Delete</button>
-                        </td>
-                    </tr>`
-                );
+                $('#table_body_manicure').append(tbodyTemplatev);
             }
 
             if (serverResponse.result[i].type === 'pedicure') {
-                $('#table_body_pedicure').append(
-                    `<tr>
-                        <td>${serverResponse.result[i].item}</td>
-                        <td>${serverResponse.result[i].description}</td>
-                        <td>${serverResponse.result[i].price}</td>
-                        <td>
-                            <button type="submit" id="edit" onclick="editService('${serverResponse.result[i]._id}')" class="btn btn-block btn-primary custom-buttom">Edit</button>
-                            <button type="submit" id="delete" onclick="deleteService('${serverResponse.result[i]._id}')" class="btn btn-block btn-danger custom-buttom">Delete</button>
-                        </td>
-                    </tr>`
-                );
+                $('#table_body_pedicure').append(tbodyTemplate);
             }
 
             if (serverResponse.result[i].type === 'extra') {
-                $('#table_body_extras').append(
-                    `<tr>
-                        <td>${serverResponse.result[i].item}</td>
-                        <td>${serverResponse.result[i].description}</td>
-                        <td>${serverResponse.result[i].price}</td>
-                        <td>
-                            <button type="submit" id="edit" onclick="editService('${serverResponse.result[i]._id}')" class="btn btn-block btn-primary custom-buttom">Edit</button>
-                            <button type="submit" id="delete" onclick="deleteService('${serverResponse.result[i]._id}')" class="btn btn-block btn-danger custom-buttom">Delete</button>
-                        </td>
-                    </tr>`
-                );
+                $('#table_body_extras').append(tbodyTemplate);
             }
         }
     }).catch(function(error){
--- views/js/table.js
+++ views/js/table.js
@@ -1,16 +1,23 @@
 
-
-function select_row() {
-    var selected = false;
-    $("#table tr").click(function ()
-    {
-        if (!selected) {
-            $(this).addClass("selected");
-            selected = true;
-        }else{
-            $(this).removeClass('selected');
-            selected = false;
-        }
-        console.log($(this));
-    })
-};
+var selected = false;
+$('[data-product-table]').on('click', 'tbody tr', function (event) {
+    if (!selected) {
+        $(this).addClass("selected");
+        selected = true;
+        console.log($(this).data("price"))
+        let price = 0;
+        $("[data-id][data-price].selected").each(function() {
+            price += $(this).data("price");
+        })
+        $("#totalPrice").text(price.toFixed(2));
+        
+    }else{
+        $(this).removeClass('selected');
+        selected = false;
+        let price = 0;
+        $("[data-id][data-price].selected").each(function() {
+            price += $(this).data("price");
+        })
+        $("#totalPrice").text(price.toFixed(2));
+    }
+});
